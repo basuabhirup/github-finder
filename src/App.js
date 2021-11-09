5dd2a0a3956579bf556ffa0dfb72f1d1114bd49e
basuabhirup <basu.abhirup@yahoo.com>
2021-11-09 21:39:21 +0530

    Convert App into Class-based component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello there</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello there</h1>
+      </div>
+    );
+  }  
 }
 
 export default App;
