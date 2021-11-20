0ef91d6f05fdf2e0a4b62bc4f75bb15e58c30c24
basuabhirup <basu.abhirup@yahoo.com>
2021-11-20 23:57:08 +0530

    Fetch users' data from GitHub Api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  componentDidMount() {
+    this.setState({ laoding: true });
+    axios
+      .get('https://api.github.com/users')
+      .then(res => this.setState({ users: res.data, loading: false }));
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
         
       </div>
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,45 +2,12 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      },
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      },
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      },
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      },
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      }
-    ]
-  }
+  
 
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}        
       </div>
