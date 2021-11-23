911ca6994f674fb6f46d0a8d7d204f86f1d4ce81
basuabhirup <basu.abhirup@yahoo.com>
2021-11-23 23:40:36 +0530

    Implement React Router DOM and create "About" page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 import axios from "axios";
 import "./App.css";
 
@@ -35,19 +37,32 @@ class App extends Component {
   const { users, loading } = this.state;
 
   return (
-   <div className="App">
-    <Navbar />
-    <div className="container">
-     <Alert alert={this.state.alert} />
-     <Search
-      searchUsers={this.searchUsers}
-      clearUsers={this.clearUsers}
-      isCleared={users.length > 0 ? true : false}
-      setAlert={this.setAlert}
-     />
-     <Users loading={loading} users={users} />
+   <Router>
+    <div className="App">
+     <Navbar />
+     <div className="container">
+      <Alert alert={this.state.alert} />
+      <Routes>
+       <Route
+        exact
+        path="/"
+        element={
+         <Fragment>
+          <Search
+           searchUsers={this.searchUsers}
+           clearUsers={this.clearUsers}
+           isCleared={users.length > 0 ? true : false}
+           setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
+         </Fragment>
+        }
+       />
+       <Route exact path="/about" element={<About />} />
+      </Routes>
+     </div>
     </div>
-   </div>
+   </Router>
   );
  }
 }
