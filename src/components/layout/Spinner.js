f73efa542747fa5f483e1c1bf727aab3c62c4dea
basuabhirup <basu.abhirup@yahoo.com>
2021-11-25 14:15:37 +0530

    Design User Component's UI & Layout

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import spinner from './spinner.gif';
+import React, { Fragment } from "react";
+import spinner from "./spinner.gif";
 
 function Spinner() {
-const spinnerStyle =  {
-  width: '200px',
-  margin: 'auto',
-  display: 'block'
+	const spinnerStyle = {
+		width: "200px",
+		margin: "auto",
+		display: "block",
+	};
+
+	return (
+		<Fragment>
+			<img src={spinner} alt="Loading..." style={spinnerStyle} />
+		</Fragment>
+	);
 }
 
-  return (
-    <>
-      <img src={spinner} alt="Loading..." style={spinnerStyle} />
-    </>
-  )
-}
-
-export default Spinner
+export default Spinner;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,5 +1,7 @@
-import React, { Component, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { Component, Fragment, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
 function User({ user, loading, getUser }) {
 	let { username } = useParams();
@@ -14,16 +16,101 @@ function User({ user, loading, getUser }) {
 		location,
 		bio,
 		blog,
+		company,
 		login,
 		html_url,
 		followers,
 		following,
-		public_reos,
+		public_repos,
 		public_gists,
 		hireable,
 	} = user;
 
-	return <div>{name}</div>;
+	if (loading) return <Spinner />;
+
+	return (
+		<Fragment>
+			<Link to="/" className="btn btn-light">
+				Back To Search
+			</Link>
+			Hireable:{" "}
+			{hireable ? (
+				<i className="fas fa-check text-success" />
+			) : (
+				<i className="fas fa-times-circle text-danger" />
+			)}
+			<div className="card grid-2">
+				<div className="all-center">
+					<img
+						src={avatar_url}
+						alt=""
+						className="round-img"
+						style={{ width: "150px" }}
+					/>
+					<h1>{name}</h1>
+					<p>Location: {location}</p>
+				</div>
+				<div>
+					{bio && (
+						<Fragment>
+							<h3>Bio</h3>
+							<p>{bio}</p>
+						</Fragment>
+					)}
+					<a
+						href={html_url}
+						className="btn btn-dark my-1"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Visit GitHub Profile
+					</a>
+					<ul>
+						<li>
+							{login && (
+								<Fragment>
+									<strong>Username: </strong> {login}
+								</Fragment>
+							)}
+						</li>
+						<li>
+							{company && (
+								<Fragment>
+									<strong>Company: </strong> {company}
+								</Fragment>
+							)}
+						</li>
+						<li>
+							{blog && (
+								<Fragment>
+									<strong>Website: </strong> {blog}
+								</Fragment>
+							)}
+						</li>
+						<li>
+							{company && (
+								<Fragment>
+									<strong>Company: </strong> {company}
+								</Fragment>
+							)}
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="card text-center">
+				<div className="badge badge-success">Followers: {followers}</div>
+				<div className="badge badge-primary">Following: {following}</div>
+				<div className="badge badge-dark">Public Repos: {public_repos}</div>
+				<div className="badge badge-light">Public Gists: {public_gists}</div>
+			</div>
+		</Fragment>
+	);
 }
+
+User.propTypes = {
+	user: PropTypes.object,
+	loading: PropTypes.bool.isRequired,
+	getUser: PropTypes.func.isRequired,
+};
 
 export default User;
